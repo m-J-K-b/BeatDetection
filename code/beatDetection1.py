3523f7c6557c43df44f6c73d95c1b6233dfa2eb0
m-J-K-b <mj@jcc-hamburg.com>
2023-11-10 22:18:43 +0100

    udpated beatDetection1

--- code/beatDetection1.py
+++ code/beatDetection1.py
@@ -37,18 +37,22 @@ def get_beat(
     local_window_size,
     instant_window_size,
     min_power_factor=1.4,
-    freq_ranges=np.array([]),
+    freq_split_positions=np.array([]),
 ):
+    instant_window_size = int(instant_window_size)
+    local_window_size = int(local_window_size)
     inverse_Fs = 1 / Fs
 
-    freq_ranges = np.concatenate((np.array([0]), freq_ranges, np.array([np.inf])))
-    freq_splits = freq_ranges.shape[-1]
+    freq_split_positions = np.concatenate(
+        (np.array([0]), freq_split_positions, np.array([np.inf]))
+    )
+    freq_split_num = freq_split_positions.shape[-1]
 
     overhead = instant_window_size - audio.shape[-1] % instant_window_size
     audio = np.concatenate((audio, np.zeros(shape=(overhead))))
 
     instant_energy_data = np.zeros(
-        shape=(freq_splits, audio.shape[-1] // instant_window_size)
+        shape=(freq_split_num, audio.shape[-1] // instant_window_size)
     )
 
     for i, data in enumerate(np.reshape(audio, newshape=(-1, instant_window_size))):
@@ -57,9 +61,9 @@ def get_beat(
             0 : instant_window_size // 2
         ]
 
-        for j in range(0, freq_splits - 1):
-            start_freq = freq_ranges[j]
-            end_freq = freq_ranges[j + 1]
+        for j in range(0, freq_split_num - 1):
+            start_freq = freq_split_positions[j]
+            end_freq = freq_split_positions[j + 1]
 
             instant_indecies = [
                 idx
@@ -70,25 +74,30 @@ def get_beat(
 
     local_normalization_factor = instant_window_size / local_window_size
 
-    W = local_window_size // instant_window_size
-    W_half = W // 2
+    window_size = local_window_size // instant_window_size
+    half_window_size = window_size // 2
     instant_energy_data_ext = np.concatenate(
         (
-            np.zeros(shape=(instant_energy_data.shape[0], W_half)),
+            np.zeros(shape=(instant_energy_data.shape[0], half_window_size)),
             instant_energy_data,
-            np.zeros(shape=(instant_energy_data.shape[0], W_half)),
+            np.zeros(shape=(instant_energy_data.shape[0], half_window_size)),
         ),
         axis=1,
     )
     instant_energy_data_strided = stride_tricks.as_strided(
         instant_energy_data_ext,
-        shape=(instant_energy_data.shape[-1], instant_energy_data.shape[0], W),
+        shape=(
+            instant_energy_data.shape[-1],
+            instant_energy_data.shape[0],
+            window_size,
+        ),
         strides=(
             instant_energy_data_ext.itemsize,
             instant_energy_data_ext.itemsize * (instant_energy_data_ext.shape[-1]),
             instant_energy_data_ext.itemsize,
         ),
     )
+
     beat = np.zeros_like(instant_energy_data)
 
     for i, local_data in enumerate(instant_energy_data_strided):
@@ -101,16 +110,20 @@ def get_beat(
             ):
                 beat[j, i] = 1
 
-    resampled_beat = np.zeros(shape=(freq_splits, Ns))
-    for i in range(freq_splits):
+    resampled_beat = np.zeros(shape=(freq_split_num, Ns))
+    for i in range(freq_split_num):
         resampled_beat[i, :] = resample_array(beat[i, :], Ns)
     return resampled_beat.copy()
 
 
 def get_time_stamps_from_beat(beat, fs, normalized=False):
-    print(beat.shape)
-    beat_indicies = np.logical_xor(beat[:, 1:], beat[:, :-1])
-    beat_indicies = np.argwhere(beat_indicies > 0) / fs
+    beat_indicies = []
+    for i in range(beat.shape[0]):
+        current_range = np.logical_xor(beat[i, :-1], beat[i, 1:])
+        a = np.arange(0, current_range.shape[-1])
+        filter_arr = current_range > 0
+        if len(current_range) != 0:
+            beat_indicies.append(a[filter_arr] / fs)
     return beat_indicies
 
 
@@ -145,7 +158,7 @@ def main():
     global done
 
     ### load audio to be analyzed
-    audio_path = str(pathlib.Path("./../assets/sounds/sampleAudio.wav").resolve())
+    audio_path = str(pathlib.Path("./../assets/sounds/simpleKickBeat.wav").resolve())
     wf = wave.open(audio_path, "r")
 
     # extract numpy array fromw wave object
@@ -156,6 +169,7 @@ def main():
     if CHANNELS == 1:
         audio_data = interleaved
     else:
+        # turn stereo to mono
         audio_data = np.reshape(interleaved, (CHANNELS, -1))
         audio_data = 0.5 * audio_data[0, :] + 0.5 * audio_data[1, :]
 
@@ -168,26 +182,27 @@ def main():
     duration_s = samples / fs
 
     ### analyze audio
-    freq_ranges = np.array([20, 60, 250, 500, 2000, 4000, 6000, 20000])
+    freq_split_positions = np.array([20, 60, 250, 500, 2000, 4000, 6000, 20000])
     # freq_ranges = np.array([])
-    freq_splits = freq_ranges.shape[0] + 1
+    freq_splits = freq_split_positions.shape[0] + 1
     beat = get_beat(
         audio=audio_data,
         Ns=samples,
         Fs=fs,
-        local_window_size=fs,
+        local_window_size=fs / 2,
         instant_window_size=1024,
-        min_power_factor=1.3,
-        freq_ranges=freq_ranges,
+        min_power_factor=5,
+        freq_split_positions=freq_split_positions,
     )
     contact_time_stamps = [
         time_stamps
         for time_stamps in get_time_stamps_from_beat(beat, fs, normalized=False)
     ]
+    for time_stamp in contact_time_stamps:
+        print(time_stamp)
     normalized_contact_time_stamps = [
         time_stamps / duration_s for time_stamps in contact_time_stamps
     ]
-    print(len(contact_time_stamps), contact_time_stamps[0].shape)
 
     ### initialize pygame
     pg.init()
@@ -213,17 +228,17 @@ def main():
         track_surface.fill((40, 40, 40))
         screen.fill((60, 60, 60))
 
-        for i, r in enumerate(freq_ranges):
+        for i, r in enumerate(freq_split_positions):
             text = ""
             if i > 0:
-                text = f"{freq_ranges[i - 1]}Hz - {r}Hz"
+                text = f"{freq_split_positions[i - 1]}Hz - {r}Hz"
             else:
                 text = f"0Hz - {r}Hz"
             rendered_text = font.render(text, True, (255, 255, 255))
             x = (WIDTH / 5 - rendered_text.get_width()) / 2
             y = (i + 0.5) / freq_splits * HEIGHT - rendered_text.get_height() / 2
             screen.blit(rendered_text, (x, y))
-        text = f"{freq_ranges[-1]}Hz - Inf Hz"
+        text = f"{freq_split_positions[-1]}Hz - Inf Hz"
         rendered_text = font.render(text, True, (255, 255, 255))
         x = (WIDTH / 5 - rendered_text.get_width()) / 2
         y = (freq_splits - 0.5) / freq_splits * HEIGHT - rendered_text.get_height() / 2
